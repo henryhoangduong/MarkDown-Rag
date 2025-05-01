064b3eb6d3d9b9ccd5cac4ca5f5d5fd376ab2e38
henryhoangduong <henryhoangduong@gmail.com>
2025-05-01 20:34:02 +0700

    add EmbeddingManager

---
+++ backend/EmbeddingManager.py
@@ -0,0 +1,19 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+class EmbeddingManager:
+    def __init__(self, documents, persist_directory, model_name="mixedbread-ai/mxbai-embed-large-v1"):
+        self.documents = documents
+        self.persist_directory = persist_directory
+        self.vectordb = None
+        self.model_name = model_name
+
+    # Method to create and persist embeddings
+    def create_and_persist_embeddings(self):
+        embedding = HuggingFaceEmbeddings(model_name=self.model_name)
+        self.vectordb = Chroma.from_documents(
+            documents=self.documents,
+            embedding=embedding,
+            persist_directory=self.persist_directory,
+        )
+        self.vectordb.persist()
